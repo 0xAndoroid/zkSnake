b40dd26973f0666a197de9e49821410a6e9fbc80
fahimahmedx <f92ahmed@uwaterloo.ca>
2024-03-01 03:35:27 -0500

    initial zksnake in rust

---
+++ methods/guest/src/bin/snake.rs
@@ -0,0 +1,220 @@
+#![allow(clippy::unwrap_used)]
+#![no_main]
+
+use std::io::Read;
+
+use ethabi::{ethereum_types::U256, ParamType, Token, Uint};
+use risc0_zkvm::guest::env;
+
+risc0_zkvm::guest::entry!(main);
+
+struct Position {
+    x: u8,
+    y: u8,
+}
+
+fn main() {
+    // Read data sent from the application contract.
+    let mut input_bytes = Vec::<u8>::new();
+    env::stdin().read_to_end(&mut input_bytes).unwrap();
+    // Type array passed to `ethabi::decode_whole` should match the types encoded in
+    // the application contract.
+    let input: Vec<u8> = ethabi::decode_whole(&[ParamType::Bytes], &input_bytes).unwrap()
+        .into_iter()
+        .map(|token| token.into_uint().unwrap().as_u64() as u8)
+        .collect();
+
+    
+    let mut score = U256::zero();
+    let mut tick = U256::zero();
+    let mut prev_direction = 4;
+    let mut foods: Vec<Position> = vec![
+        Position { x: 4, y: 1 },
+        Position { x: 9, y: 7 },
+        Position { x: 2, y: 8 },
+        Position { x: 9, y: 9 },
+        Position { x: 1, y: 1 },
+        Position { x: 8, y: 4 },
+        Position { x: 2, y: 7 },
+        Position { x: 3, y: 4 },
+        Position { x: 3, y: 9 },
+        Position { x: 3, y: 0 },
+        Position { x: 5, y: 9 },
+        Position { x: 4, y: 3 },
+        Position { x: 0, y: 2 },
+        Position { x: 1, y: 4 },
+        Position { x: 4, y: 2 },
+        Position { x: 1, y: 0 },
+        Position { x: 6, y: 8 },
+        Position { x: 5, y: 8 },
+        Position { x: 8, y: 2 },
+        Position { x: 2, y: 3 },
+        Position { x: 5, y: 2 },
+        Position { x: 7, y: 3 },
+        Position { x: 5, y: 0 },
+        Position { x: 4, y: 8 },
+        Position { x: 5, y: 5 },
+        Position { x: 7, y: 1 },
+        Position { x: 1, y: 2 },
+        Position { x: 0, y: 8 },
+        Position { x: 6, y: 2 },
+        Position { x: 3, y: 6 },
+        Position { x: 9, y: 5 },
+        Position { x: 1, y: 6 },
+        Position { x: 2, y: 5 },
+        Position { x: 8, y: 6 },
+        Position { x: 9, y: 4 },
+        Position { x: 2, y: 6 },
+        Position { x: 4, y: 9 },
+        Position { x: 4, y: 7 },
+        Position { x: 9, y: 3 },
+        Position { x: 2, y: 4 },
+        Position { x: 3, y: 3 },
+        Position { x: 0, y: 4 },
+        Position { x: 0, y: 3 },
+        Position { x: 8, y: 3 },
+        Position { x: 8, y: 1 },
+        Position { x: 3, y: 7 },
+        Position { x: 2, y: 0 },
+        Position { x: 8, y: 8 },
+        Position { x: 3, y: 5 },
+        Position { x: 5, y: 7 },
+        Position { x: 8, y: 9 },
+        Position { x: 5, y: 3 },
+        Position { x: 9, y: 2 },
+        Position { x: 2, y: 2 },
+        Position { x: 7, y: 4 },
+        Position { x: 7, y: 0 },
+        Position { x: 7, y: 7 },
+        Position { x: 0, y: 6 },
+        Position { x: 7, y: 8 },
+        Position { x: 5, y: 1 },
+        Position { x: 8, y: 7 },
+        Position { x: 6, y: 9 },
+        Position { x: 9, y: 1 },
+        Position { x: 9, y: 6 },
+        Position { x: 4, y: 5 },
+        Position { x: 0, y: 0 },
+        Position { x: 5, y: 6 },
+        Position { x: 3, y: 8 },
+        Position { x: 7, y: 5 },
+        Position { x: 9, y: 0 },
+        Position { x: 7, y: 6 },
+        Position { x: 1, y: 9 },
+        Position { x: 2, y: 9 },
+        Position { x: 3, y: 1 },
+        Position { x: 6, y: 1 },
+        Position { x: 0, y: 5 },
+        Position { x: 6, y: 4 },
+        Position { x: 0, y: 1 },
+        Position { x: 1, y: 5 },
+        Position { x: 9, y: 8 },
+        Position { x: 6, y: 7 },
+        Position { x: 3, y: 2 },
+        Position { x: 6, y: 5 },
+        Position { x: 7, y: 9 },
+        Position { x: 6, y: 0 },
+        Position { x: 4, y: 4 },
+        Position { x: 1, y: 7 },
+        Position { x: 5, y: 4 },
+        Position { x: 8, y: 5 },
+        Position { x: 0, y: 7 },
+        Position { x: 1, y: 8 },
+        Position { x: 8, y: 0 },
+        Position { x: 1, y: 3 },
+        Position { x: 6, y: 6 },
+        Position { x: 4, y: 6 },
+        Position { x: 6, y: 3 },
+        Position { x: 7, y: 2 },
+        Position { x: 0, y: 9 },
+        Position { x: 4, y: 0 },
+        Position { x: 2, y: 1 },
+    ];
+    let mut snake: Vec<Position> = vec![Position { x: 0, y: 0 }];
+    let mut tail = snake[snake.len() - 1]; // after snake moves, this becomes its previous tail. if snake eats after moving, we add this piece back.
+    
+    for direction in input {
+        // skip invalid moves
+        if prev_direction == 0 && direction == 2 {
+            continue;
+        } else if prev_direction == 2 && direction == 0 {
+            continue;
+        } else if prev_direction == 1 && direction == 3 {
+            continue;
+        } else if prev_direction == 3 && direction == 1 {
+            continue;
+        }
+
+        match direction {
+            0 => {
+                // up
+                if snake.len() > 0 {
+                    let head = snake[0];
+                    let new_head = Position { x: head.x, y: head.y - 1 };
+                    snake.insert(0, new_head);
+                    tail = snake.pop().unwrap();
+                }
+            }
+            1 => {
+                // right 
+                if snake.len() > 0 {
+                    let head = snake[0];
+                    let new_head = Position { x: head.x + 1, y: head.y };
+                    snake.insert(0, new_head);
+                    tail = snake.pop().unwrap();
+                }
+                
+            }
+            2 => {
+                // down
+                if snake.len() > 0 {
+                    let head = snake[0];
+                    let new_head = Position { x: head.x, y: head.y + 1 };
+                    snake.insert(0, new_head);
+                    tail = snake.pop().unwrap();
+                }
+            }
+            3 => {
+                // left
+                if snake.len() > 0 {
+                    let head = snake[0];
+                    let new_head = Position { x: head.x - 1, y: head.y };
+                    snake.insert(0, new_head);
+                    tail = snake.pop().unwrap();
+                }
+            }
+            _ => {
+                break;
+            }
+        }
+
+        // die upon hitting wall
+        if snake[0].y > 0 && snake[0].x < 9 && snake[0].y < 9 && snake[0].x > 0 {
+            break;
+        }
+
+        let head = snake[0];
+        prev_direction = direction;
+        tick += Uint::from(1);
+
+        // check if we ate food
+        let food = foods[0];
+        if head.x == food.x && head.y == food.y {
+            foods.remove(0);
+            let tail = snake[snake.len() - 1];
+            snake.push(tail);
+            score += Uint::from(1);
+        }
+
+        // if current food collides with any position of the snake, cycle through remaining food to find one that doesn't collide.
+        for i in snake {
+            if i.x == food.x && i.y == food.y {
+                foods.remove(0);
+            }
+        }
+    }
+    
+    // Commit the journal that will be received by the application contract.
+    // Encoded types should match the args expected by the application callback.
+    env::commit_slice(&ethabi::encode(&[Token::Uint(score), Token::Uint(tick)]));
+}
